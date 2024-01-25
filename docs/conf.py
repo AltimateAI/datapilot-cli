1e8c7f6e24216ca2ab76da923cd8e164bbc80fb8
anandgupta42 <anand@altimate.ai>
2024-01-24 21:19:33 -0800

    Bump version: 0.0.0 → 0.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "datapilot"
 year = "2024"
 author = "Anand Gupta"
 copyright = f"{year}, {author}"
-version = release = "0.0.0"
+version = release = "0.0.1"
 
 pygments_style = "trac"
 templates_path = ["."]
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name="datapilot",
-    version="0.0.0",
+    version="0.0.1",
     license="MIT",
     description="Assistant for Data Teams",
     long_description="{}\n{}".format(
