450129117c9897cda092a906b823b2dfdbb5491d
Pulkit Gaur <gaurpulkit99@gmail.com>
2024-02-28 14:23:29 +0530

    setup change

--- setup.py
+++ setup.py
@@ -74,7 +74,5 @@ setup(
         #   "rst": ["docutils>=0.11"],
         #   ":python_version=="2.6"": ["argparse"],
     },
-    entry_points={
-        "console_scripts": ["datapilot = datapilot.cli.main:datapilot", "executor = src.datapilot.core.platforms.dbt.executor:main"]
-    },
+    entry_points={"console_scripts": ["datapilot = datapilot.cli.main:datapilot", "executor = datapilot.core.platforms.dbt.executor:main"]},
 )
