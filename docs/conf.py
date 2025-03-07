2791088ea60ab5c1f5a6edf73ccf8d9aebf20e16
anandgupta42 <anand@altimate.ai>
2025-03-07 06:13:13 -0800

    Bump version: 0.0.16 → 0.0.17

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "datapilot-cli"
 year = "2024"
 author = "Altimate Inc."
 copyright = f"{year}, {author}"
-version = release = "0.0.16"
+version = release = "0.0.17"
 
 pygments_style = "trac"
 templates_path = ["."]
