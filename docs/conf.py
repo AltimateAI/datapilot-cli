f432c97ce17b7b662f48f634fa54d129af76764c
suryaiyer95 <surya@altimate.ai>
2025-06-12 01:48:03 -0700

    Bump version: 0.0.20 → 0.0.21

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "datapilot-cli"
 year = "2024"
 author = "Altimate Inc."
 copyright = f"{year}, {author}"
-version = release = "0.0.20"
+version = release = "0.0.21"
 
 pygments_style = "trac"
 templates_path = ["."]
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name="altimate-datapilot-cli",
-    version="0.0.20",
+    version="0.0.21",
     license="MIT",
     description="Assistant for Data Teams",
     long_description="{}\n{}".format(
