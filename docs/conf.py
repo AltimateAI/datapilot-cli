5f679abd9dbda48a48b4c9a065f7a5ddb946e577
anandgupta42 <93243293+anandgupta42@users.noreply.github.com>
2025-06-11 22:49:21 -0700

    Bump version: 0.0.19 → 0.0.20 (#56)

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "datapilot-cli"
 year = "2024"
 author = "Altimate Inc."
 copyright = f"{year}, {author}"
-version = release = "0.0.19"
+version = release = "0.0.20"
 
 pygments_style = "trac"
 templates_path = ["."]
