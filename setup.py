01e6a25262fe35514cd47f0bf68e6330c47c9333
Pulkit Gaur <30400626+gaurpulkit@users.noreply.github.com>
2025-08-01 09:19:55 +0530

    Bump version: 0.0.21 → 0.0.22 and update dbt-artifacts-parser dependency to 0.9.0. Add support for ManifestV12 in DBTVersion union. (#63)

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name="altimate-datapilot-cli",
-    version="0.0.21",
+    version="0.0.22",
     license="MIT",
     description="Assistant for Data Teams",
     long_description="{}\n{}".format(
@@ -63,7 +63,7 @@ setup(
     python_requires=">=3.8",
     install_requires=[
         "click~=8.1.7",
-        "dbt-artifacts-parser~=0.8.1",
+        "dbt-artifacts-parser~=0.9.0",
         "ruamel.yaml~=0.18.6",
         "tabulate~=0.9.0",
         "requests>=2.31",
