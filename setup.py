f950999f0e221df9c9756b1d668f7953929713b4
Michiel De Smet <mdesmet@gmail.com>
2025-10-02 16:37:35 -0700

    fix: add pydantic dependency

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ setup(
     python_requires=">=3.8",
     install_requires=[
         "click~=8.1.7",
-        "dbt-artifacts-parser~=0.9.0",
+        "pydantic >=2.0,<3.0",
         "ruamel.yaml~=0.18.6",
         "tabulate~=0.9.0",
         "requests>=2.31",
