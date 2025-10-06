56f7af4790590eb7e6a5c96c704eb0e7f202fe5a
anandgupta42 <anand@altimate.ai>
2025-10-06 10:27:45 -0700

    bunping version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name="altimate-datapilot-cli",
-    version="0.0.24",
+    version="0.0.25",
     license="MIT",
     description="Assistant for Data Teams",
     long_description="{}\n{}".format(
