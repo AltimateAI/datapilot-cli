fe87a4d534a8056a3c718b6864f96fa05e8a5484
Sourabhchrs93 <Sourabhchrs93@users.noreply.github.com>
2026-04-06 20:06:02 +0000

    chore: bump version from 0.2.2 to 0.2.3

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "datapilot-cli"
 year = "2024"
 author = "Altimate Inc."
 copyright = f"{year}, {author}"
-version = release = "0.2.2"
+version = release = "0.2.3"
 
 pygments_style = "trac"
 templates_path = ["."]
