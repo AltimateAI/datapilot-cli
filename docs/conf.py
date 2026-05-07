0d3758446ec6a4f9594c85719fc12137a25b3871
suryaiyer95 <surya@altimate.ai>
2026-05-06 22:36:46 -0700

    chore: bump version from 0.2.3 to 0.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ project = "datapilot-cli"
 year = "2024"
 author = "Altimate Inc."
 copyright = f"{year}, {author}"
-version = release = "0.2.3"
+version = release = "0.3.0"
 
 pygments_style = "trac"
 templates_path = ["."]
