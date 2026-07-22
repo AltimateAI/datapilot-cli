563f0d41a91d03364875b5b6b84578a86bbbc901
Sourabhchrs93 <Sourabhchrs93@users.noreply.github.com>
2026-07-22 18:59:58 +0000

    chore: bump version from 0.3.3 to 0.3.4

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(*names, **kwargs):
 
 setup(
     name="altimate-datapilot-cli",
-    version="0.3.3",
+    version="0.3.4",
     license="MIT",
     description="Assistant for Data Teams",
     long_description="{}\n{}".format(
